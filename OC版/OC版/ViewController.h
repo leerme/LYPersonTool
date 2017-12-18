b45bee082de150efa1612631e7a252a7649184f6
leerme <leerme@163.com>
2017-12-18 10:17:06 +0800

    LYRollingNoticeDemo

--- OC版/OC版/ViewController.h
+++ OC版/OC版/ViewController.h
@@ -1,15 +1,12 @@
 //
 //  ViewController.h
-//  OC版
+//  
 //
-//  Created by jjs on 2017/12/14.
-//  Copyright © 2017年 iOS-LeiYu. All rights reserved.
+//  Created by jjs on 2017/12/18.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
-
 @end
-
