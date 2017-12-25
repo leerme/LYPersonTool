f3f65396e671942f44b828deb9fcfac447922236
leerme <642459351@qq.com>
2017-12-25 22:11:30 +0800

    添加仿支付宝demo

---
+++ OC版/OC版/LYAliPayCellMoveDemo/LYAliPayCellMoveController.h
@@ -0,0 +1,13 @@
+//
+//  LYAliPayCellMoveController.h
+//  OC版
+//
+//  Created by jjs on 2017/12/25.
+//  Copyright © 2017年 iOS-LeiYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYAliPayCellMoveController : UIViewController
+
+@end
---
+++ OC版/OC版/LYAliPayCellMoveDemo/LYAliPayCellMoveController.m
@@ -0,0 +1,37 @@
+//
+//  LYAliPayCellMoveController.m
+//  OC版
+//
+//  Created by jjs on 2017/12/25.
+//  Copyright © 2017年 iOS-LeiYu. All rights reserved.
+//
+
+#import "LYAliPayCellMoveController.h"
+
+@interface LYAliPayCellMoveController ()
+
+@end
+
+@implementation LYAliPayCellMoveController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
