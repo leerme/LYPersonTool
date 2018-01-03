e76a7be8e478991e44c854d491eabdc788b06c94
leerme <642459351@qq.com>
2018-01-03 23:11:53 +0800

    SlideMenuView

--- OC版/OC版/LYSlideMenuDemo/SlideMenuView/SlideMenuButton.m
+++ OC版/OC版/LYSlideMenuDemo/SlideMenuView/SlideMenuButton.m
@@ -27,6 +27,19 @@
 - (void)drawRect:(CGRect)rect{
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextAddRect(context, rect);
+    [self.buttonColor set];
+    CGContextFillPath(context);
+    
+    UIBezierPath *roundedRectanglePath = [UIBezierPath bezierPathWithRoundedRect:CGRectInset(rect, 1, 1) cornerRadius:rect.size.height/2];
+    [self.buttonColor setFill];
+    [roundedRectanglePath fill];
+    [[UIColor whiteColor] setStroke];
+    roundedRectanglePath.lineWidth = 1;
+    [roundedRectanglePath stroke];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle defaultParagraphStyle] mutableCopy];
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    NSDictionary *attr = @{NSParagraphStyleAttributeName:paragraphStyle,NSFontAttributeName:[UIFont systemFontOfSize:24.0f],NSForegroundColorAttributeName:[UIColor whiteColor]};
 }
 
 
